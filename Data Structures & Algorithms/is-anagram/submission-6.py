403d1b82d68e6eab038d9b4dd090ccbedc10d929
Harsh Nevse <110392719+HarshNevse@users.noreply.github.com>
2026-09-09 22:45:19 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # 'harsh' ==> {'h': 2, 'a': 1, 'r': 1, 's': 1}
+
+        s_counter = defaultdict()
+        t_counter = defaultdict()
+
+        for i in s:
+            s_counter[i] = s_counter.get(i, 0) + 1
+        for i in t:
+            t_counter[i] = t_counter.get(i, 0) + 1
+
+        return s_counter == t_counter
+
+        
+
+        
