11698c1db1c8e53e41079d716db491b10eaacfb7
Harsh Nevse <110392719+HarshNevse@users.noreply.github.com>
2026-09-09 22:48:53 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,19 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # 'harsh' ==> {'h': 2, 'a': 1, 'r': 1, 's': 1}
+
+        s_counter = {}
+        t_counter = {}
+
+        for i in s:
+            s_counter[i] = s_counter.get(i, 0) + 1
+        for i in t:
+            t_counter[i] = t_counter.get(i, 0) + 1
+
+        return s_counter == t_counter
+        # return Counter(s) == Counter(t)
+
+        
+
+        
